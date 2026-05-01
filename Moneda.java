1370a392be7394febf2bdce57d4ff47f51649126
soulsilver142 <joelrojas2025@udec.cl>
2026-05-01 15:12:20 -0400

    feat: agregar subclases de moneda

--- Moneda.java
+++ Moneda.java
@@ -1,4 +1,4 @@
-abstract public class Moneda {
+abstract class Moneda {
     public Moneda(){
     }
 
@@ -6,4 +6,48 @@ abstract public class Moneda {
         return this;
     }
     public abstract int getValor();
+}
+
+class Moneda1500 extends Moneda {
+    public Moneda1500(){
+        super();
+    }
+
+    @Override
+    public int getValor(){
+        return 1500;
+    }
+}
+
+class Moneda1000 extends Moneda {
+    public Moneda1000(){
+        super();
+    }
+
+    @Override
+    public int getValor(){
+        return 1000;
+    }
+}
+
+class Moneda500 extends Moneda {
+    public Moneda500(){
+        super();
+    }
+
+    @Override
+    public int getValor(){
+        return 500;
+    }
+}
+
+class Moneda100 extends Moneda {
+    public Moneda100(){
+        super();
+    }
+
+    @Override
+    public int getValor(){
+        return 100;
+    }
 }
