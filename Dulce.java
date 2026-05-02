089ef4dfd29617f3386a2533d8a3fedcd112f664
soulsilver142 <joelrojas2025@udec.cl>
2026-05-02 19:23:25 -0400

    style: modificar sabores en minúscula

--- Dulce.java
+++ Dulce.java
@@ -15,17 +15,14 @@ public abstract class Dulce extends Producto {
     public abstract String comer();
 }
 
-
 class Super8 extends Dulce{
     public Super8 (int serie) {
         super(serie);
     }
     @Override
     public String comer(){
-        return "Super8";
+        return "super8";
     }
-
-
 }
 
 class Snickers extends Dulce{
@@ -34,7 +31,6 @@ class Snickers extends Dulce{
     }
     @Override
     public String comer(){
-        return "Snickers";
+        return "snickers";
     }
-
 }
