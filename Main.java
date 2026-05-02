ab510e789c6a396cbaf99fe9bb3339d1f1defbbf
soulsilver142 <joelrojas2025@udec.cl>
2026-05-02 19:46:38 -0400

    docs: añadir javadoc en clase main

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
+/** Clase main en la cual se prueban los casos posibles y también las excepciones */
 public class Main {
-	// Se definen las constantes para cada producto
 	public static final int  COCA=1;
 	public static final int  SPRITE=2;
 	public static final int  FANTA=3;
 	public static final int  SUPER8=4;
 	public static final int  SNICKERS=5;
 
+	/**
+	 * @throws NoHayProductoException
+	 * @throws PagoIncorrectoException
+	 * @throws PagoInsuficienteException
+	 */
 	public static void main() throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
-		// Se crean los expendedores para probar las excepciones
 		Expendedor expendedorValido = new Expendedor(3);
 		Expendedor expendedorNulo = new Expendedor(0);
 
-		// Adicionalmente se crean las monedas
 		Moneda monedaNula = null;
 		Moneda monedaInferior = new Moneda100();
 		Moneda monedaValida = new Moneda1500();
@@ -34,25 +37,18 @@ public class Main {
 			System.out.println("Error: No hay productos disponibles en el deposito");
 		}
 
-		// Desde aquí se crean los compradores, cada uno con la intención de comprar un producto distinto
-
-		// Persona que compra una bebida, específicamente una sprite
 		Moneda monedaSprite = new Moneda1000();
 		Comprador personaCocaCola = new Comprador(monedaSprite, SPRITE, expendedorValido);
 		System.out.println(personaCocaCola.queConsumiste() + " " + personaCocaCola.cuantoVuelto());
 
-		// Persona que compra un dulce, específicamente un super 8
 		Moneda monedaSuperOcho = new Moneda500();
 		Comprador personaSuperOcho = new Comprador(monedaSuperOcho, SUPER8, expendedorValido);
 		System.out.println(personaSuperOcho.queConsumiste() + " " + personaSuperOcho.cuantoVuelto());
 
-		// Persona que compra un dulce, específicamente un snickers
 		Moneda monedaSnickers = new Moneda1500();
 		Comprador personaSnickers = new Comprador(monedaSnickers, SNICKERS, expendedorValido);
 		System.out.println(personaSnickers.queConsumiste() + " " + personaSnickers.cuantoVuelto());
 
-		// Desde aquí se prueba el método toString para algunas monedas
-		// se pueden ver los distintos números de serie
 		System.out.println("Moneda Nula: " + monedaNula);
 		System.out.println("Moneda Inferior: " + monedaInferior);
 		System.out.println("Moneda Valida: " + monedaValida);
