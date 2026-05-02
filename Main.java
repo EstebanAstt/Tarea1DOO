0145d95b43ca6bc180cb1a9ac343206509fe2ec3
soulsilver142 <joelrojas2025@udec.cl>
2026-05-02 01:24:53 -0400

    feat: añadir excepciones no definitivas en main

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
 public class Main {
-	public static void main(){
+	public static void main() throws Exception{
+		Expendedor exp = new Expendedor(10);
+		Moneda m = null;
+		// agregar comprador
+
+		try {
+			exp.comprarProducto(m, 1);
+		} catch (PagoIncorrectoException e) {
+			System.out.println("No se ingreso ninguna moneda");
+		} catch (PagoInsuficienteException e) {
+			System.out.println("El valor de moneda es inferior al producto");
+		} catch (NoHayProductoException e) {
+			System.out.println("No hay producto en el deposito");
+		}
 	}
 }
