7b7ae99dfa1eac99c6234b54538caeb07216149b
soulsilver142 <joelrojas2025@udec.cl>
2026-05-02 17:06:09 -0400

    feat: agregar moneda en clase main

--- Main.java
+++ Main.java
@@ -1,17 +1,6 @@
 public class Main {
-	public static void main() throws Exception{
-		Expendedor exp = new Expendedor(10);
-		Moneda m = null;
-		// agregar comprador
-
-		try {
-			exp.comprarProducto(m, 1);
-		} catch (PagoIncorrectoException e) {
-			System.out.println("No se ingreso ninguna moneda");
-		} catch (PagoInsuficienteException e) {
-			System.out.println("El valor de moneda es inferior al producto");
-		} catch (NoHayProductoException e) {
-			System.out.println("No hay producto en el deposito");
-		}
+	public static void main() throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
+		Moneda m;
+		m = new Moneda100();
 	}
 }
