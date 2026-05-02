aa6cc30869cd7f8475543df04e397be2bd8cc97d
soulsilver142 <joelrojas2025@udec.cl>
2026-05-02 18:57:02 -0400

    feat: ingresar nuevamente las excepciones en main

--- Main.java
+++ Main.java
@@ -1,12 +1,32 @@
 public class Main {
 	public static void main() throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
-		Moneda m = new Moneda1500();
-		System.out.println(m);
+		// Se crea un expendedor
+		Expendedor expendedor = new Expendedor(5);
 
-		Moneda mDos = new Moneda100();
-		System.out.println(mDos);
+		// Primero se prueban las excepciones relacionadas al pago
+		Moneda monedaUno = null;
+		Moneda monedaDos = new Moneda100();
 
-		Moneda mTres = new Moneda500();
-		System.out.println(mTres);
+		try {
+			expendedor.comprarProducto(monedaUno, 1);
+		} catch (PagoIncorrectoException e) {
+			System.out.println("Error: No se ingresó ninguna moneda");
+		}
+
+		try {
+			expendedor.comprarProducto(monedaDos, 1);
+		} catch (PagoInsuficienteException e) {
+			System.out.println("Error: El valor de la moneda ingresada es inferior al producto deseado");
+		}
+
+		// Se prueba la última excepción con un expendedor sin ningún producto
+		Expendedor expendedorNulo = new Expendedor(0);
+		Moneda monedaValida = new Moneda1500();
+
+		try {
+			expendedorNulo.comprarProducto(monedaValida, 1);
+		} catch (NoHayProductoException e) {
+			System.out.println("Error: No hay productos disponibles en el deposito");
+		}
 	}
 }
