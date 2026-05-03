eeeb7c182be2f90b9356449e89d3c42ba1d7fd07
soulsilver142 <joelrojas2025@udec.cl>
2026-05-02 23:31:48 -0400

    docs: añadir javadoc a clase comprador

--- Comprador.java
+++ Comprador.java
@@ -1,13 +1,22 @@
+/** Clase la cual corresponde al pedido del comprador */
 public class Comprador {
     private String sonido;
     private int vuelto;
 
+    /**
+     * Método constructor, recibe moneda, tipo de producto y un expendedor
+     * Determina el vuelto al recibir las monedas del deposito
+     * @param m
+     * @param tipo
+     * @param exp
+     * @throws PagoIncorrectoException
+     * @throws PagoInsuficienteException
+     * @throws NoHayProductoException
+     */
     public Comprador(Moneda m, int tipo, Expendedor exp) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {
         this.vuelto = 0;
-        
         Producto producto = exp.comprarProducto(m, tipo);
-        
-        // si se pudo comprar, se consume
+
         if (producto instanceof Bebida bebida) {
             this.sonido = bebida.beber();
         } else if (producto instanceof Dulce dulce){
@@ -16,17 +25,24 @@ public class Comprador {
             this.sonido = null;
         }
 
-        // saca el vuelto moneda a moneda hasta que quede vacio
         Moneda monedaVuelto;
         while ((monedaVuelto = exp.getVuelto()) != null) {
             this.vuelto += monedaVuelto.getValor();
         }
     }
 
+    /**
+     * Retorna el valor total de la suma de las monedas del deposito
+     * @return valor del vuelto
+     */
     public int cuantoVuelto() {
         return this.vuelto;
     }
 
+    /**
+     * Retorna el producto
+     * @return string correspondiente al producto que se compró
+     */
     public String queConsumiste() {
         return this.sonido;
     }
